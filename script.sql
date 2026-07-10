a2de08bf02174c917c67edf4f3ca49539fdcba07
hardy-chan <hardychan69@gmail.com>
2026-07-10 18:46:56 +0200

    Remove testing script from remote repository

--- script.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    session_id AS "Session ID", 
-    message_id AS "Message ID", 
-    role AS "Sender", 
-    content AS "Message Content", 
-    TO_CHAR(created_at, 'YYYY-MM-DD HH24:MI:SS') AS "Timestamp"
-FROM SYSTEM.chat_history 
-ORDER BY session_id ASC, created_at ASC;
